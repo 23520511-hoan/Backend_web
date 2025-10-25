f3fac20fe1596e0ded469d69c68ed8830567524c
23520511-hoan <23520511@gm.uit.edu.vn>
2025-10-25 12:43:44 +0700

    tạo category cho models

---
+++ models/Category.js
@@ -0,0 +1,40 @@
+// models/Category.js
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a category name'],
+    unique: true,
+    trim: true,
+    maxlength: [100, 'Category name cannot be more than 100 characters']
+  },
+  slug: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  description: {
+    type: String,
+    maxlength: [500, 'Description cannot be more than 500 characters']
+  },
+  image: {
+    type: String,
+    default: 'default-category.jpg'
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+categorySchema.pre('save', function(next) {
+  if (this.isModified('name')) {
+    this.slug = this.name.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
+  }
+  next();
+});
+
+module.exports = mongoose.model('Category', categorySchema);
