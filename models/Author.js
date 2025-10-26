f403c49d21a3025e12dddd44c31124015b6169e2
23520511-hoan <23520511@gm.uit.edu.vn>
2025-10-26 22:05:33 +0700

    sửa author

--- models/Author.js
+++ models/Author.js
@@ -1,39 +1,42 @@
 // models/Author.js
+const mongoose = require('mongoose');
+
 const authorSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'Please provide an author name'],
-      trim: true,
-      maxlength: [100, 'Author name cannot be more than 100 characters']
-    },
-    biography: {
-      type: String,
-      maxlength: [2000, 'Biography cannot be more than 2000 characters']
-    },
-    dateOfBirth: {
-      type: Date
-    },
-    nationality: {
-      type: String
-    },
-    avatar: {
-      type: String,
-      default: 'default-author.jpg'
-    },
-    website: {
-      type: String
-    },
-    socialMedia: {
-      facebook: String,
-      twitter: String,
-      instagram: String
-    },
-    isActive: {
-      type: Boolean,
-      default: true
-    }
-  }, {
-    timestamps: true
-  });
-  
-  module.exports = mongoose.model('Author', authorSchema);
+  name: {
+    type: String,
+    required: [true, 'Please provide an author name'],
+    trim: true,
+    maxlength: [100, 'Author name cannot be more than 100 characters'],
+    index: true // Thêm index để tối ưu hóa tìm kiếm
+  },
+  biography: {
+    type: String,
+    maxlength: [2000, 'Biography cannot be more than 2000 characters']
+  },
+  dateOfBirth: {
+    type: Date
+  },
+  nationality: {
+    type: String
+  },
+  avatar: {
+    type: String,
+    default: 'default-author.jpg'
+  },
+  website: {
+    type: String
+  },
+  socialMedia: {
+    facebook: String,
+    twitter: String,
+    instagram: String
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Author', authorSchema);
